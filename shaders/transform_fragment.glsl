54df12e597ae919d706743778e52c9eae15293cf
Zhiming Guo <mjzshd@gmail.com>
2014-09-26 15:19:35 +0800

    Remove shaders

--- shaders/transform_fragment.glsl
+++
@@ -1,9 +0,0 @@
-#version 120
-attribute vec4 vertex_position;
-uniform mat4 M; // This is model transformation 
-uniform sampler2D tex;
-
-void main()  {
-    vec4 color = texture2D(tex, gl_TexCoord[0].st);
-    gl_Position = M * vertex_position / vertex_position.w; 
-}
