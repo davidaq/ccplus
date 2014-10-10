981ec45f72e47630e152207a6843677dc1f0b00c
DavidAQ <anqin@ccme.cc>
2014-10-10 11:13:01 +0800

    clean

--- include/zip.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-extern "C" {
-    int compress(unsigned char *pDest, unsigned long *pDest_len, const unsigned char *pSource, unsigned long source_len);
-    
-    int uncompress(unsigned char *pDest, unsigned long *pDest_len, const unsigned char *pSource, unsigned long source_len);
-}
