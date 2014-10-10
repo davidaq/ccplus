ab2d0c2397c53ec7140f475c9f6b6c121137a2b0
DavidAQ <anqin@ccme.cc>
2014-10-10 13:56:58 +0800

    rewind unchanged sources

---
+++ src/parallel-executor.cpp
@@ -0,0 +1,75 @@
+#include "parallel-executor.hpp"
+#include "logger.hpp"
+
+using namespace CCPlus;
+
+void* ParallelExecutor::executeFunc(void* ctx) {
+    ParallelExecutor& executor = *((ParallelExecutor*)ctx);
+    while(true) {
+        sem_wait(&executor.listSemaphore);
+        std::function<void() > job;
+        bool run = false;
+        pthread_mutex_lock(&executor.jobListLock);
+        if(!executor.jobList.empty()) {
+            run = true;
+            job = executor.jobList.front();
+            executor.jobList.pop_front();
+        }
+        pthread_mutex_unlock(&executor.jobListLock);
+        if(run) {
+            job();
+        } else if(executor.ended) {
+            break;
+        }
+    }
+    return 0;
+}
+
+ParallelExecutor::ParallelExecutor(int threadCount) {
+    extraThreadsCount = threadCount - 1;
+    pthread_mutex_init(&jobListLock, 0);
+    sem_init(&listSemaphore, 0, 0);
+    if(extraThreadsCount > 0) {
+        extraThreads = new pthread_t[extraThreadsCount];
+        for(int i = 0; i < extraThreadsCount; i++) {
+            if (pthread_create(extraThreads + i, 0, 
+                        ParallelExecutor::executeFunc, this)) {
+                log(logFATAL) << "Failed to create new thread";
+            }
+        }
+    }
+}
+
+ParallelExecutor::~ParallelExecutor() {
+    waitForAll();
+    if(extraThreads)
+        delete [] extraThreads;
+}
+
+void ParallelExecutor::execute(std::function<void()> job) {
+    bool runNow = false;
+    pthread_mutex_lock(&jobListLock);
+    if(jobList.size() < extraThreadsCount * 2) {
+        jobList.push_back(job);
+        sem_post(&listSemaphore);
+    } else
+        runNow = true;
+    pthread_mutex_unlock(&jobListLock);
+    if(runNow)
+        job();
+}
+
+void ParallelExecutor::waitForAll() {
+    if(ended)
+        return;
+    ended = true;
+    // Activate other sleeping thread
+    for(int i = 0; i < extraThreadsCount; i++)
+        sem_post(&listSemaphore);
+    // Main thread start running
+    sem_post(&listSemaphore);
+    executeFunc(this);
+    for(int i = 0; i < extraThreadsCount; i++) 
+        pthread_join(extraThreads[i], 0);
+}
+
