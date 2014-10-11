5a7cda85670a2c4daa33c1764acc0482e21cad0a
Zhiming Guo <mjzshd@gmail.com>
2014-10-11 11:01:46 +0800

    image renderable prepare

--- src/image-renderable.cpp
+++ src/image-renderable.cpp
@@ -1,6 +1,7 @@
 #include "image-renderable.hpp"
 
 using namespace CCPlus;
+using namespace cv;
 
 ImageRenderable::ImageRenderable(const std::string& uri) {
     this->uri = uri;
@@ -11,11 +12,48 @@ Frame ImageRenderable::getFrame(float) {
 }
 
 float ImageRenderable::getDuration() {
-    return 1;
+    return 100000;
 }
 
 void ImageRenderable::prepare() {
-    image.image = cv::imread(parseUri2File(uri));
+    Mat org = cv::imread(parseUri2File(uri));
+    if (org.channels() == 3) {
+        cv::Mat newimg = cv::Mat(org.rows, org.cols, CV_8UC4, {0, 0, 0, 255});
+        int from_to = {0, 0, 1, 1, 2, 2};
+        mixChannels(&org, 1, &newimg, 1, from_to, 3);
+        org = newimg;
+    }
+
+    auto rotateCWRightAngle = [&org] (int angle) {
+        if (angle % 90 != 0) {
+            log(logWARN) << "Unexpected rotation angle: " << angle;
+        }
+
+        if (angle == 180) {
+            flip(org, org, -1); 
+        } else if (angle == 90) {
+            transpose(org, org);
+            flip(org, org, 1); 
+        } else if (angle == 270) {
+            transpose(org, org);
+            flip(org, org, 0); 
+        }
+    }; 
+
+    if (stringEndsWith(toLower(filepath), ".jpg")) {
+        try {
+            int degree = getImageRotation(filepath);
+            if (degree == -1) {
+                log(logWARN) << "Failed getting image rotationg angle";
+            } else {
+                rotateCWRightAngle(degree);
+            }
+        } catch (...) {
+            log(logWARN) << "Failed getting image rotationg angle";
+        }
+    }
+
+    image.image = org;
 }
 
 void ImageRenderable::release() {
