aee4fac48b129e647a3599fcfd6e1e18c5c1e7fa
Zhiming Guo <mjzshd@gmail.com>
2014-10-15 14:55:59 +0800

    BIG CHANGE

---
+++ src/gpu-frame-impl.cpp
@@ -0,0 +1,37 @@
+#include "gpu-frame-impl.hpp"
+#include "frame.hpp"
+#include "profile.hpp"
+#include "gpu-frame-cache.hpp"
+
+using namespace CCPlus;
+
+GPUFrameImpl::~GPUFrameImpl() {
+    GPUFrameCache::reuse(this);
+}
+
+void GPUFrameImpl::bindFBO() {
+    glBindFramebuffer(GL_FRAMEBUFFER, fboID);
+    glViewport(0, 0, width, height);
+}
+
+Frame GPUFrameImpl::toCPU() {
+    Frame ret;
+    ret.ext = ext;
+    if(textureID) {
+        glBindTexture(GL_TEXTURE_2D, textureID);
+        ret.image = cv::Mat::zeros(height, width, CV_8UC4);
+        bindFBO();
+        glReadPixels(0, 0, width, height, GL_BGRA_EXT, GL_UNSIGNED_BYTE, ret.image.data);
+        glFinish();
+    }
+    return ret;
+}
+
+void GPUFrameImpl::load(const Frame& frame) {
+    ext = frame.ext;
+    if(width * height > 0) {
+        glBindTexture(GL_TEXTURE_2D, textureID);
+        glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, width, height, 
+                GL_BGRA_EXT, GL_UNSIGNED_BYTE, frame.image.data);
+    }
+}
