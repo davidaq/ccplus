fbc881e4b29be83065d1c4d52490f65a6cf29311
Zhiming Guo <mjzshd@gmail.com>
2014-10-16 11:19:32 +0800

    Improve gaussian

--- assets/shaders/filters/gaussian.v.glsl
+++ assets/shaders/filters/gaussian.v.glsl
@@ -3,29 +3,32 @@
 attribute vec4 vertex_position;
 
 varying vec2 xy;
-varying vec2 nbs[9];
+varying vec2 nbs[25];
+varying float cnt;
 
 uniform vec2 pixelOffset;
 uniform int ksize;
+uniform float gOffsets[100];
 
 void main() {
-    const float gOffsets[9] = float[9](
-        0.66293,
-        2.47904,
-        4.46232,
-        6.44568,
-        8.42917,
-        10.41281,
-        12.39664,
-        14.38070,
-        16.36501
-    );
-    for(int i = 0; i < 9; i++) {
-        vec2 texCoordOffset = gOffsets[i] * pixelOffset;
-        nbs[i] = texCoordOffset;
-    }; 
-
     gl_Position = vertex_position;
     xy = gl_Position.xy * 0.5 + 0.5;
-
+    //const float gOffsets[9] = float[9](
+    //    0.66293,
+    //    2.47904,
+    //    4.46232,
+    //    6.44568,
+    //    8.42917,
+    //    10.41281,
+    //    12.39664,
+    //    14.38070,
+    //    16.36501
+    //);
+    cnt = ksize;
+    for(int i = 0; i < cnt; i++) {
+        vec2 texCoordOffset = gOffsets[i] * pixelOffset;
+        nbs[i] = texCoordOffset;
+        //nbs[i] = xy + texCoordOffset;
+        //nbs[ksize + i] = xy - texCoordOffset;
+    }; 
 }
