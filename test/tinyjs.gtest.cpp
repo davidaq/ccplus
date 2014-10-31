fa6f8d622edab859021b2930eaf5e4a6de20f80c
DavidAQ <aq@num16.com>
2014-10-31 23:13:04 +0800

    tinyjs test

--- test/tinyjs.gtest.cpp
+++ test/tinyjs.gtest.cpp
@@ -32,47 +32,17 @@ TEST(TinyJS, ReturnJSON) {
     CTinyJS js;
     registerFunctions(&js);
     EVAL("var tmlObj = {hello: 'hello world'}; var ret = JSON.stringify(tmlObj, null);");
+    EXPECT_EQ("{ \n  \"hello\" : \"hello world\"\n}", js.evaluate("ret"));
 }
 
 TEST(TinyJS, LoadJSON) {
     const std::string json = "{\"key\" : \"value\"}";
-    const std::string code = "var tmlObj = eval(json);";
+
     CTinyJS js;
     registerFunctions(&js);
-
     js.root->addChild("json", new CScriptVar(json));
 
-    try {
-        js.execute(code);
-        //L() << js.evaluate("JSON.stringify(tmlObj, null)");
-        L() << js.evaluate("tmlObj.key");
-    } catch(CScriptException* e) {
-        L() << e->text;
-    }
+    EVAL("var tmlObj = JSON.parse(json);");
+    EXPECT_EQ("value", js.evaluate("tmlObj.key"));
 }
 
-TEST(TinyJS, Loop) {
-    const std::string code = "var obj = [1, 2, 3]; for (var i = 0; i < 3; i++) obj[i]++;";
-    CTinyJS js;
-    registerFunctions(&js);
-
-    try {
-        js.execute(code);
-        L() << js.evaluate("obj[0]");
-    } catch(CScriptException* e) {
-        L() << e->text;
-    }
-}
-
-TEST(TinyJS, TEST) {
-    const std::string code = "var obj = {x: 1, y: 2};";
-    CTinyJS js;
-    registerFunctions(&js);
-
-    try {
-        js.execute(code);
-        L() << js.evaluate("keys(obj)");
-    } catch(CScriptException* e) {
-        L() << e->text;
-    }
-}
