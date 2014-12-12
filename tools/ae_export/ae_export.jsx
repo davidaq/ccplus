169b528b6a4f225931173c174998e69d3671f9e4
DavidAQ <anqin@ccme.cc>
2014-12-12 13:37:54 +0800

    corner pin export

--- tools/ae_export/ae_export.jsx
+++ tools/ae_export/ae_export.jsx
@@ -528,6 +528,23 @@ mapProperty({
 });
 
 /*******
+ * src/propertymap/cornerpin.js
+ *******/
+mapProperty({
+    name: '4corner',
+    order: 1,
+    map:[
+        'Effects/Corner Pin/Upper Left',
+        'Effects/Corner Pin/Upper Right',
+        'Effects/Corner Pin/Lower Left',
+        'Effects/Corner Pin/Lower Right',
+    ],
+    set:function(ul, ur, ll, lr) {
+        return [ul[0], ul[1], ur[0], ur[1], ll[0], ll[1], lr[0], lr[1]];
+    }
+});
+
+/*******
  * src/propertymap/gaussian.js
  *******/
 mapProperty({
