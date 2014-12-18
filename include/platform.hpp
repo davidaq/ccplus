66df1d8fbc6609d8f14bc5b61ee989671dea55ee
DavidAQ <anqin@ccme.cc>
2014-12-18 15:37:09 +0800

    player interface

--- include/platform.hpp
+++ include/platform.hpp
@@ -1,6 +1,10 @@
 #pragma once
 
 namespace CCPlus {
+    // OpenGL Context
     void* createGLContext();
     void destroyGLContext(void*);
+
+    // delete file
+    void removeFile(const std::string& fileName, bool removeDir=false);
 }
