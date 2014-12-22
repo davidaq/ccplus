e93c7217765806aaeb494e63c9735b19ab111897
apple <mjzshd@gmail.com>
2014-12-22 14:48:58 +0800

    Fix black & white problem

--- src/filters/grayscale.cpp
+++ src/filters/grayscale.cpp
@@ -10,6 +10,9 @@ CCPLUS_FILTER(grayscale) {
         log(logERROR) << "Insufficient parameters for grayscale effect";
         return frame;
     }
+    if (parameters[7] && parameters[6]) {
+        log(logWARN) << "Tint color's effect is not guaranteed.";
+    }
     GLProgramManager* manager = GLProgramManager::getManager();
     GLuint weightsU, hue_satU;
     GLuint program = manager->getProgram(filter_grayscale, &weightsU, &hue_satU);
@@ -18,11 +21,12 @@ CCPLUS_FILTER(grayscale) {
     GPUFrame ret = GPUFrameCache::alloc(frame->width, frame->height);
     ret->bindFBO(false);
 
-    static float weights[6];
+    static float weights[7];
     for (int i = 0; i < 6; i++) 
         weights[i] = parameters[i] / 100.0;
+    weights[6] = weights[0];
 
-    glUniform1fv(weightsU, 6, weights);
+    glUniform1fv(weightsU, 7, weights);
     glUniform2f(hue_satU, parameters[6] / 360.0f, parameters[7]);
     glActiveTexture(GL_TEXTURE0);
     glBindTexture(GL_TEXTURE_2D, frame->textureID);
